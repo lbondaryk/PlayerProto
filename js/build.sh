369b4e86ce75d1ea98d4b24ba57ab9c6be6f9aa1
Mike Lippert <mlippert255@gmail.com>
2013-08-16 10:41:44 -0400

    initial script for running the closurebuilder
    This helps because there are a lot of options and it would be a pain to type them
    all, or even remember which ones I'm currently trying to use.

---
+++ js/build.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+BUILDER="/home/mjl/Projects/pearson/closure/closure-library/closure/bin/build/closurebuilder.py"
+COMPILER="/home/mjl/Projects/pearson/closure/closure-compiler/compiler.jar" 
+LIBRARYDIR="/home/mjl/Projects/pearson/closure/closure-library/" 
+OUTFILE="brixlib-compiled.js"
+
+declare -a COMPILER_ARGS=(\
+	'--compilation_level=SIMPLE_OPTIMIZATIONS'\
+	'--source_map_format=V3'\
+	"--create_source_map=${OUTFILE}.map"\
+	"--language_in=ECMASCRIPT5_STRICT"\
+	"--warning_level=VERBOSE"\
+	)
+
+BRIX_ARGS=$(cat <<EOF
+	--input=eventmanager.js
+	--input=widget-base.js
+	--input=widget-image.js
+	--input=widget-carousel.js
+	--input=widget-imageviewer.js
+	--root=.
+	--root=$LIBRARYDIR
+	--output_mode=compiled
+	--output_file=$OUTFILE
+	--compiler_jar=$COMPILER
+	${COMPILER_ARGS[*]/#/--compiler_flags=}
+EOF
+)
+
+$BUILDER $BRIX_ARGS
