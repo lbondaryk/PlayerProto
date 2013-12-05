539b09bd349a39d3e87cf211ffa61a4110115a0c
seannives <seann.ives@gmail.com>
2013-12-05 11:32:40 -0500

    json samples for paf

--- test/integration/pafjson/addingtopaf-samples.sh
+++ test/integration/pafjson/addingtopaf-samples.sh
@@ -67,3 +67,30 @@ java -jar brix-tool-pafclient-0.1-jar-with-dependencies.jar -m PUT \
 # To retrieve: notice the -m GET
 java -jar brix-tool-pafclient-0.1-jar-with-dependencies.jar -m GET \
 	-u http://repo.paf.cert.pearsoncmg.com/paf-repo/resources/activities/test.sanvan.assign.mcq1b -c
+
+##########
+# Journal Question
+
+# Add Activity
+# @id: http://repo.paf.cert.pearsoncmg.com/paf-repo/resources/activities/7CCE2F41-D1A5-40CD-B2B6-DFF7E8731801
+java -jar brix-tool-pafclient-0.1-jar-with-dependencies.jar -m POST \
+	-h "Content-Type: application/vnd.pearson.paf.v1.envelope+json;body=\"application/vnd.pearson.sanvan.v1.activity\"\"" \
+	-d journal/sanvan_journal_item_1.activity.json \
+	-u http://repo.paf.cert.pearsoncmg.com/paf-repo/resources/activities -c
+
+# Add Assignment
+# @id: http://repo.paf.cert.pearsoncmg.com/paf-repo/resources/activities/4872CF3F-276B-443B-A1B9-3FB95D187E62
+java -jar brix-tool-pafclient-0.1-jar-with-dependencies.jar -m POST \
+	-h "Content-Type: application/vnd.pearson.paf.v1.envelope+json;body=\"application/vnd.pearson.paf.v1.assignment+json\"\"" \
+	-d journal/sanvan_journal.assign.json \
+	-u http://repo.paf.cert.pearsoncmg.com/paf-repo/resources/activities -c
+
+# Get Activity
+java -jar brix-tool-pafclient-0.1-jar-with-dependencies.jar -m GET \
+    -h "Accept:application/vnd.pearson.sanvan.v1.activity" \
+    -u http://repo.paf.cert.pearsoncmg.com/paf-repo/resources/activities/7CCE2F41-D1A5-40CD-B2B6-DFF7E8731801 -c
+
+# Get Assignment
+java -jar brix-tool-pafclient-0.1-jar-with-dependencies.jar -m GET \
+    -h "Accept:application/vnd.pearson.paf.v1.assignment+json" \
+    -u http://repo.paf.cert.pearsoncmg.com/paf-repo/resources/activities/4872CF3F-276B-443B-A1B9-3FB95D187E62 -c	
